edcab45a33b2180425771beaff5f216897cf4233
Teguh Dana Prayuda <megtrotech@gmail.com>
2022-06-21 12:32:36 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-blue-700 mx">Teguh dana prayuda</h1>
+    </div>
+  );
+}
+
+export default App;
